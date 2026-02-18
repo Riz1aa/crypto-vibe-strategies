2e32705b295cdf7b0ab3eb25044cba2984bc89b2
Illia Brailovskyi <illiabrailovskiy@gmail.com>
2026-02-18 12:55:48 +0000

    Create config.py with strategy configuration

---
+++ config.py
@@ -0,0 +1,8 @@
+# Strategy Configuration
+
+CVD_lookback = 5
+OI_lookback = 5
+OI_expansion_threshold = 0.0020  # 0.20%
+price_stagnation_threshold = 0.0010  # 0.10%
+ATR_period = 14
+assets = ['BTC/USDT', 'ETH/USDT']
